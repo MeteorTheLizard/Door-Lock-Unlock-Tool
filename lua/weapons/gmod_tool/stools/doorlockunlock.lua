bdfc0f3761dc64cbf754e506b670998f64d0d6df
MeteorTheLizard <55365852+MeteorTheLizard@users.noreply.github.com>
2020-08-24 20:54:52 +0200

    Formatting improvments + slight clientside optimization

--- lua/weapons/gmod_tool/stools/doorlockunlock.lua
+++ lua/weapons/gmod_tool/stools/doorlockunlock.lua
@@ -2,50 +2,51 @@ TOOL.Category = "Construction"
 TOOL.Name = "#Tool.doorlockunlock.name"
 
 if CLIENT then
-    language.Add("Tool.doorlockunlock.name", "Door Lock/Unlock")
-    language.Add("Tool.doorlockunlock.desc", "Lock or Unlock Doors.")
-    language.Add("Tool.doorlockunlock.left", "Primary: Lock")
-    language.Add("Tool.doorlockunlock.right", "Secondary: Unlock")
+	function TOOL.BuildCPanel(CPanel)
+		CPanel:AddControl("Header",{
+			Description = "#tool.doorlockunlock.desc"
+		})
+	end
+
+	language.Add("Tool.doorlockunlock.name","Door Lock/Unlock")
+	language.Add("Tool.doorlockunlock.desc","Lock or Unlock Doors.")
+	language.Add("Tool.doorlockunlock.left","Primary: Lock")
+	language.Add("Tool.doorlockunlock.right","Secondary: Unlock")
 end
 
 TOOL.Information = {
-    {
-        name = "left",
-    },
-    {
-        name = "right",
-    }
+	{
+		name = "left"
+	},
+	{
+		name = "right"
+	}
 }
 
 function TOOL:LeftClick(trace)
-    local Ent = trace.Entity
-    if not IsValid(Ent) then return end
+	local Ent = trace.Entity
+	if not IsValid(Ent) then return end
 
-    if SERVER then
-        Ent:Fire("lock")
-        Ent:EmitSound("doors/latchlocked2.wav", 75, 100, 1, CHAN_AUTO)
-    end
+	if SERVER then
+		Ent:Fire("lock")
+		Ent:EmitSound("doors/latchlocked2.wav")
+	end
 
-    return false
+	return false
 end
 
 function TOOL:RightClick(trace)
-    local Ent = trace.Entity
-    if not IsValid(Ent) then return end
+	local Ent = trace.Entity
+	if not IsValid(Ent) then return end
 
-    if SERVER then
-        Ent:Fire("unlock")
-        Ent:EmitSound("doors/latchunlocked1.wav", 75, 100, 1, CHAN_AUTO)
-    end
+	if SERVER then
+		Ent:Fire("unlock")
+		Ent:EmitSound("doors/latchunlocked1.wav")
+	end
 
-    return false
+	return false
 end
 
 function TOOL:Reload()
-end
 
-function TOOL.BuildCPanel(CPanel)
-    CPanel:AddControl("Header", {
-        Description = "#tool.doorlockunlock.desc"
-    })
 end
